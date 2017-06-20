b23a655e23286f07c37555d6ed72d200af19d44f
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-06-20 20:06:19 +0000

    resolve conflict

--- python/mxnet/module/module.py
+++ python/mxnet/module/module.py
@@ -576,7 +576,6 @@ class Module(BaseModule):
         if self._update_on_kvstore:
             _update_params_on_kvstore(self._exec_group.param_arrays,
                                       self._exec_group.grad_arrays,
-                                      self._kvstore,
                                       self._kvstore, self._exec_group.param_names)
         else:
             _update_params(self._exec_group.param_arrays,
